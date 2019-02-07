3e17a7211d4c2c82065eb99e343937d0f9c354e7
Bradley Gray <bradley.gray+github@bradleygray.co.uk>
2019-02-07 17:30:22 +0000

    django 2.x compat and user relation

--- dj4xol/urls.py
+++ dj4xol/urls.py
@@ -1,6 +1,10 @@
-from django.conf.urls import url
+try:
+    from django.conf.urls import url
+except ImportError:
+    from django.urls import re_path as url
 
 from . import views
+
 
 urlpatterns = [
     url(r'^$', views.starmap, name='map')
