f5ed1768e44f0a230f0c016cc6572b4ec090801f
Bradley Gray <bradley.gray+github@bradleygray.co.uk>
2019-02-07 17:30:22 +0000

    django 2.x compat and user relation

--- dj4xol/models.py
+++ dj4xol/models.py
@@ -1,24 +1,29 @@
 from django.db import models
+from django.contrib.auth import models as auth_models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Player(models.Model):
-    firstname = models.CharField(max_length=30)
-    lastname = models.CharField(max_length=30)
-    alias = models.CharField(max_length=30)
+    django_user = models.OneToOneField(auth_models.User, primary_key=True,
+            related_name="dj4xolplayer", on_delete = models.PROTECT)
+    full_name = models.CharField(max_length=60)
+    alias = models.CharField(max_length=30, unique=True)
     email = models.EmailField()
 
 
 class Game(models.Model):
     name = models.CharField(max_length=30)
     players = models.ManyToManyField(Player, related_name="games")
+    owner = models.ForeignKey(Player, related_name="mygames",
+            on_delete=models.CASCADE)
     description = models.TextField()
     map_size_x = models.IntegerField()
     map_size_y = models.IntegerField()
 
 
 class AbstractGameObject(models.Model):
-    game = models.ForeignKey(Game, related_name="%(class)ss")
+    game = models.ForeignKey(Game, related_name="%(class)ss",
+            on_delete=models.CASCADE)
 
     class Meta:
         abstract = True
@@ -33,22 +38,26 @@ class AbstractMapObject(AbstractGameObject):
 
 
 class Ship(AbstractMapObject):
-    player = models.ForeignKey(Player, related_name = 'ships')
+    player = models.ForeignKey(Player, related_name = 'ships',
+            on_delete=models.CASCADE)
 
 
 class Star(AbstractMapObject):
     name = models.CharField(max_length=30)
-    player = models.ForeignKey(Player, null=True, default=None, related_name = 
-                               'stars')
+    player = models.ForeignKey(Player, null=True, default=None, related_name =
+            'stars', on_delete=models.CASCADE)
 
 
 class ShipOrders(AbstractGameObject):
-    ship = models.ForeignKey(Ship, related_name="orders")
+    ship = models.ForeignKey(Ship, related_name="orders",
+            on_delete=models.CASCADE)
     repeat = models.BooleanField(default = False)
     warpfactor = models.IntegerField(default = 0, 
                                      validators=[MinValueValidator(0), MaxValueValidator(13)])
     x = models.IntegerField(null=True)
     y = models.IntegerField(null=True)
-    target_star = models.ForeignKey(Star, null=True, related_name='+')
-    target_ship = models.ForeignKey(Ship, null=True, related_name='+')
+    target_star = models.ForeignKey(Star, null=True, related_name='+',
+            on_delete=models.CASCADE)
+    target_ship = models.ForeignKey(Ship, null=True, related_name='+',
+            on_delete=models.CASCADE)
 
