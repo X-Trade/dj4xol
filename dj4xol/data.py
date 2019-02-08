a02a8b7c3497b3d276a6d8c9081ae3710c0c693d
Bradley Gray <bradley.gray+github@bradleygray.co.uk>
2019-02-08 19:51:26 +0000

    star names, item selection from map

--- dj4xol/data.py
+++ dj4xol/data.py
@@ -1,8 +1,23 @@
-from .models import Game
+from .models import Game, Star
+import random
 import numpy as np
 
 
-class GameTurn:
+class GameFactory():
+    def __init__(self, game):
+        self.starnamer = StarNamer()
+        self.game = game
+
+    def add_stars(self, stars):
+        for _ in range(stars):
+            x = random.randint(1, self.game.map_size_x - 1)
+            y = random.randint(1, self.game.map_size_y - 1)
+            name = self.starnamer.get_unique()
+            star = Star(game=self.game, name=name, x=x, y=y)
+            star.save()
+
+
+class GameTurn():
     def __init__(self, game):
         self.game = game
 
@@ -54,3 +69,107 @@ class GameTurn:
                 ship.x = int(new_position[0])
                 ship.y = int(new_position[1])
             ship.save()
+
+
+class StarNamer():
+    data = ['Eridanus', 'Eridani', 'Acamar', 'Cassiopeia', 'Achird', 'Scorpius',
+            'Acrab', 'Scorpii', 'Crux', 'Crucis', 'Acrux', 'Cancer', 'Cancri',
+            'Acubens', 'Leo', 'Leonis', 'Leodis', 'Adhafera', 'Canis', 'Adhara',
+            'Andromeda', 'Andromedae', 'Adhil', 'Taurus', 'Tauri', 'Ain',
+            'Sagittarius', 'Sagittarii', 'Ainalrami', 'Lyra', 'Lyrae',
+            'Aladfar', 'Alamak', 'Albaldah', 'Aquarius', 'Aquarii', 'Albali',
+            'Cygnus', 'Cygni', 'Albireo', 'Corvus', 'Corvi', 'Alchiba', 'Ursa',
+            'Ursae', 'Alcor', 'Alcyone', 'Aldebaran', 'Cepheus', 'Cephei',
+            'Grus', 'Gruis', 'Alderamin', 'Draco', 'Draconis', 'Delphinus',
+            'Delphini', 'Cepheus', 'Cephei', 'Capricorn', 'Capricornus',
+            'Capricorni', 'Pegasus', 'Pegasi', 'Leod', 'Leodi', 'Leodis',
+            'Algieba', 'Perseus', 'Persei', 'Algol', 'Gemini', 'Geminorum',
+            'Alioth', 'Crateris', 'Auriga', 'Alkes', 'Almaaz', 'Almach', 'Grus',
+            'Gruis', 'Orion', 'Orinis', 'Scorpius', 'Hydra', 'Corona',
+            'Alphecca', 'Pisces', 'Piscium', 'Alrakis', 'Alpherg', 'Lynx',
+            'Lyncis', 'Vela', 'Velorum', 'Aquila', 'Aquilae', 'Alsafi',
+            'Alshain', 'Alshat', 'Altair', 'Serpens', 'Serpentis', 'Alya',
+            'Australis', 'Borealis', 'Phoenix', 'Phoenicis', 'Aquarius',
+            'Aquarii', 'Ancha', 'Ankaa', 'Anser', 'Antares', 'Arcturus',
+            'Arkab', 'Lepus', 'Leporis', 'Hydrae', 'Bootes', 'Puppis', 'Carina',
+            'Carinae', 'Athebyne', 'Atlas', 'Atria', 'Triangulum', 'Trianguli',
+            'Avior', 'Aires', 'Arietis', 'Libra', 'Bellatrix', 'Betelgeuse',
+            'Aries', 'Arietis', 'Caph', 'Capella', 'Canopus', 'Castor',
+            'Castula', 'Ophiuchus', 'Cebalrai', 'Arae', 'Chalawan', 'Chamukuy',
+            'Cervantes', 'Canes', 'Venatici', 'Canum', 'Venaticorum', 'Chara',
+            'Charon', 'Bob', 'Chertan', 'Copernicus', 'Cor', 'Caroli', 'Cujam',
+            'Cursa', 'Hercules', 'Herculis', 'Dalim', 'Fornax', 'Fornacis',
+            'Deneb', 'Cetus', 'Coma', 'Berenices', 'Diadem', 'Ceti', 'Dubhe',
+            'Virgo', 'Kubrick', 'Clarke', 'Columba', 'Columbia', 'Columbae',
+            'Electra', 'Virginis', 'Errai', 'Enif', 'Eris', 'Elgafar', 'Edmund',
+            'Garnet', 'Fomalhaut', 'Helvetios', 'Centaurus', 'Centauri', 'Izar',
+            'Hadar', 'Hades', 'Haedus', 'Hell', 'Dark', 'Valhalla', 'Paradise',
+            'Heze', 'Kaus', 'Keid', 'Herculaneum', 'Hel', 'Hej', 'Frost',
+            'Hoth', 'Alderan', 'Izar', 'Maia', 'Lost', 'Found', 'Maia', 'Ice',
+            'Eagle', 'Libertas', 'Marfark', 'Meissa', 'Meleph', 'Medusa',
+            'Merak', 'Menkar', 'Menkent', 'London', 'Mesarthim', 'Mimosa',
+            'Mira', 'Mirach', 'Miram', 'Mirzam', 'Muphrid', 'Musica', 'Goliath',
+            'Sparta', 'Dante', 'Rana', 'Rama', 'Heka', 'Hoedus', 'Tyr', 'Tyl',
+            'Tip', 'Top', 'Freedom', 'Lush', 'Fire', 'Wolf', 'Smith', 'Coronae',
+            'Libra', 'Librae', 'Chomsky', 'Atlas', 'Apache', 'Io', 'Europa',
+            'Ganymede', 'Callisto', 'Proxima', 'Makemake', 'Haumea', 'Mercury',
+            'Soil', 'Kreutz', 'Kalt', 'Hale', 'Bopp', 'Oort', 'Kuiper', 'Sedna',
+            'Vulcan', 'Vulcanoid', 'Vega', 'Apex', 'Sirius', 'Lalande', 'Cloud',
+            'Wise', 'Tau', 'Luyten', 'Ross', 'Nine', 'Six', 'Four', 'Three',
+            'Two', 'Titan', 'Titania', 'Rhea', 'Oberon', 'Vesta', 'Ariel',
+            'Umbriel', 'Ida', 'Hyperion', 'Mimas', 'Triton', 'Tethys', 'Janus',
+            'Phoebe', 'Lutetia', 'Pandora', 'Pandorum', 'Pandoria', 'Helene',
+            'Mathilde', 'Doom', 'Dare', 'Tetra', 'Karma', 'Chasma', 'Mini',
+            'Toriaz', 'Nova', 'Peak', 'Tempus', 'Tepid', 'Warm', 'Hubris',
+            'Baxter', 'Reading', 'Bristol', 'York', 'Yorke', 'Hawking',
+            'Helium', 'Dandy', 'Dorf', 'Silent', 'Red', 'Yellow', 'Orange',
+            'Blue', 'Fizz', 'Adriana', 'Beata', 'Charmaine', 'Clarina',
+            'Claris', 'Elyse', 'Elysium', 'Elvira', 'Joy', 'Sorrow', 'July',
+            'Rome', 'Roma', 'Kiev', 'Nitra', 'Kremnica', 'Martin', 'Frankfurt',
+            'Dresden', 'Chemnitz', 'Leipzig', 'Arnhem', 'Anaheim', 'Lille',
+            'Paris', 'Nantes', 'Reims', 'Metz', 'Ipswitch', 'Dover', 'Margate',
+            'Luton', 'Vik', 'Hoffell', 'Hofn', 'Dalvik', 'Davros', 'Reykholt',
+            'Hella', 'Fjord', 'Polaris', 'Ross', 'Clyde', 'Resolute', 'Leuven',
+            'Bruges', 'Tampico', 'Xalapa', 'Tula', 'Gonzalez', 'Tulum',
+            'Panama', 'Belezas', 'Urucu', 'Ipixuna', 'Nile', 'Lux', 'Luxor',
+            'LV-n', 'NGC-n', 'M-n', 'A-n', 'X-n', 'Messier']
+    additional = ['Alpha', 'Beta', 'Delta', 'Epsilon', 'Gamma']
+    suffixes = additional + ['Major', 'Minor', 'Star', 'Object', 'Majoris']
+    history = []
+
+    def _random_name(self):
+        chance = random.randint(0,12)
+        print(chance)
+        index = random.randint(0, len(self.data)-1)
+        name = self.data[index]
+        if name.endswith('-n'):
+            suffix = str(random.randint(49,499))
+            return name[:-1] + suffix
+        elif chance < 1:
+            suffix = str(random.randint(49,499))
+            return "%s-%s" % (name, suffix)
+        elif chance <= 3:
+            suffix = self.suffixes[random.randint(0, len(self.suffixes)-1)]
+            return "%s %s" % (name, suffix)
+        elif chance == 4:
+            prefix = self.data[random.randint(0, len(self.data)-1)]
+            if prefix.endswith('-n'):
+                return self._random_name()
+            return "%s %s" % (prefix, name)
+        else:
+            return name
+
+
+    def get(self):
+        name = self._random_name()
+        self.history.append(name)
+        return name
+
+    def get_unique(self):
+        while True:
+            name = self._random_name()
+            if not name in self.history:
+                break
+
+        self.history.append(name)
+        return name
--- dj4xol/views.py
+++ dj4xol/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
+from django.urls import resolve
+from itertools import chain
 
-from .models import Game
+from .models import Game, Ship, Star
 from .decorators import player_only_view
 from .data import GameTurn
 
@@ -11,12 +13,55 @@ def starmap(request, game_id):
     A rudimentary map viewer.
     """
     game = Game.objects.get(pk=game_id)
+    url = request.path
+
+    x = request.GET.get('x', None)
+    y = request.GET.get('y', None)
+    selected = request.GET.get('sel', None)
+    if selected:
+        if selected.startswith('star') or selected.startswith('ship'):
+            sel_id = int(selected[4:])
+            sel_class = selected[:4]
+            if sel_class == 'star':
+                selected = game.stars.get(pk=sel_id)
+            elif sel_class == 'ship':
+                selected = game.ships.get(pk=sel_id)
+            x = selected.x
+            y = selected.y
+
+    detail = ''
+    if x and y:
+        x = int(x)
+        y = int(y)
+        stars = game.stars.filter(x=x, y=y).all()
+        ships = game.ships.filter(x=x, y=y).all()
+        at_cursor = chain(stars, ships)
+        if not selected:
+            selected = stars[0]
+        detail = "<div id='detail'><h2>%s</h2> at %i,%i " % (selected.name, x, y)
+        if selected.player:
+            detail += "owner: " + selected.player.django_user.username
+        for item in at_cursor:
+            if isinstance(item, Star):
+                identifier = "%s%i" % ('star', item.pk)
+            elif isinstance(item, Ship):
+                identifier = "%s%i" % ('ship', item.pk)
+            detail += "<li><a href=\"%s?sel=%s\">%s</a></li>" % (url,
+                    identifier, item.name)
+
 
     gamemap = [['&nbsp' for _ in range(game.map_size_y + 1)] for _ in
             range(game.map_size_x + 1)]
 
     for star in game.stars.all():
-        gamemap[star.x][star.y] = '+'
+        if x == star.x and y == star.y:
+            color = 'blue'
+        elif star.player == request.user.dj4xolplayer:
+            color = 'green'
+        else:
+            color = 'white'
+        gamemap[star.x][star.y] = '<a style="color:%s;text-decoration:none;" \
+                                  href=%s?x=%i&y=%i>+</a>' % (color, url, star.x, star.y)
     for ship in game.ships.all():
         if gamemap[ship.x][ship.y] == '+':
             gamemap[ship.x][ship.y] = '*'
@@ -26,11 +71,12 @@ def starmap(request, game_id):
     html = "<html><body>"
     html += "<h1>%s</h1>" % (game.name)
     html += "<p>%s</p>" % (game.description)
-    html += '<h2>Starmap</h2><div style="height:600px;width:600px;border:1px solid #ccc;background-color:black;color:white;font-family:monospace;overflow:auto;">'
+    html += '<div id=\'map\'><h2>Starmap</h2><div style="height:600px;width:600px;border:1px solid #ccc;background-color:black;color:white;font-family:monospace;overflow:auto;font-size:10px;">'
     for x in gamemap:
         for y in x:
             html += y
         html += "<br />"
-    html += "</div>"
+    html += "</div></div>"
+    html += detail
     html += "</body></html>"
     return HttpResponse(html)
