878dd83ee3bdf514fc5e6091cdb9a850db37e939
Bradley Gray <bradley.gray@bradleygray.co.uk>
2023-07-13 17:53:33 +0100

    create random ships (for testing)

--- dj4xol/factory.py
+++ dj4xol/factory.py
@@ -1,26 +1,34 @@
 from dj4xol.starnamer import StarNamer
-from .models import Game, Star, Player
+from .models import Game, Star, Ship, Player
 import random
 
 class GameFactory():
     def __init__(self, game = None):
         self.starnamer = StarNamer()
         self.stars = []
+        self.ships = []
         if game:
             self.game = game
         else:
             self.game = Game()
 
     def save(self):
+        """Save the game and all stars to the database."""
         self.game.save()
         for star in self.stars:
             star.game = self.game
         Star.objects.bulk_create(self.stars)
+        for ship in self.ships:
+            ship.game = self.game
+        Ship.objects.bulk_create(self.ships)
+        self.game.save()
 
     def set_owner(self, owner):
+        """Set the owner of the game. The owner is the first player to join the game."""
         if not isinstance(owner, Player):
             raise TypeError("owner is not an instance of the Player model object")
         self.game.owner = owner
+        self.game.players.add(owner)
 
     def set_map_size(self, x, y):
         self.game.map_size_x = x
@@ -35,6 +43,7 @@ class GameFactory():
             return self._create_random_stars(stars)
 
     def _create_random_stars(self, stars):
+        """Create stars randomly in the game."""
         min_x = 1
         min_y = 1
         max_x = self.game.map_size_x - 1
@@ -46,6 +55,7 @@ class GameFactory():
             self.stars.append(Star(name=name, x=x, y=y))
 
     def _create_star_clusters(self, stars, system_size=8):
+        """Create stars in clusters, each with a maximum number of stars."""
         min_x = 1
         min_y = 1
         max_x = self.game.map_size_x - 1
@@ -62,6 +72,13 @@ class GameFactory():
                 y = cluster_y + ofs_y
                 self.stars.append(Star(name=name, x=x, y=y))
                 created += 1
-
-
+    
+    def _create_random_ships(self, ships):
+        """Create ships for a player and place them randomly in the game. Used mainly for testing purposes."""
+        for player in self.game.players.all():
+            for _ in range(ships):
+                name = self.starnamer.get_unique()
+                x = random.randint(1, self.game.map_size_x)
+                y = random.randint(1, self.game.map_size_y)
+                self.ships.append(Ship(name=name, x=x, y=y, player=player))
 
