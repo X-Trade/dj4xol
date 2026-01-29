746eea6491ef165008b99550045f1b9e4d1ed4e8
Bradley Gray <bradley.gray@bradleygray.co.uk>
2026-01-29 21:14:08 +0000

    added detail view

--- dj4xol/static/dj4xol/starmap/js/mapscrollpersist.js
+++ dj4xol/static/dj4xol/starmap/js/mapscrollpersist.js
@@ -1,18 +1,42 @@
-$("document").ready( function() {
- var posX = localStorage.getItem('posX');
- $("#starmap").scrollLeft(posX);
- var posY = localStorage.getItem('posY');
- $("#starmap").scrollTop(posY);
- console.log("ready");
- console.log(posX);
- console.log(posY);
+$("document").ready(function() {
+    var $starmap = $("#starmap");
 
-$(window).bind('beforeunload', function() {
- var posX = ($("#starmap").scrollLeft());
- var posY = ($("#starmap").scrollTop());
- localStorage.setItem('posX', posX);
- localStorage.setItem('posY', posY);
- console.log("click");
- console.log(posX);
- console.log(posY);
-}); });
+    // Restore scroll position from localStorage
+    var posX = localStorage.getItem('posX');
+    var posY = localStorage.getItem('posY');
+    $starmap.scrollLeft(posX);
+    $starmap.scrollTop(posY);
+
+    // Save scroll position before page unload
+    $(window).bind('beforeunload', function() {
+        localStorage.setItem('posX', $starmap.scrollLeft());
+        localStorage.setItem('posY', $starmap.scrollTop());
+    });
+
+    // Click+drag scrolling
+    var isDragging = false;
+    var startX, startY, scrollLeft, scrollTop;
+
+    $starmap.on('mousedown', function(e) {
+        isDragging = true;
+        startX = e.pageX - $starmap.offset().left;
+        startY = e.pageY - $starmap.offset().top;
+        scrollLeft = $starmap.scrollLeft();
+        scrollTop = $starmap.scrollTop();
+    });
+
+    $(document).on('mouseup', function() {
+        isDragging = false;
+    });
+
+    $(document).on('mousemove', function(e) {
+        if (!isDragging) return;
+        e.preventDefault();
+        var x = e.pageX - $starmap.offset().left;
+        var y = e.pageY - $starmap.offset().top;
+        var walkX = (x - startX);
+        var walkY = (y - startY);
+        $starmap.scrollLeft(scrollLeft - walkX);
+        $starmap.scrollTop(scrollTop - walkY);
+    });
+});
