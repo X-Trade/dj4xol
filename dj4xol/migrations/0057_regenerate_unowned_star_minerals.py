5c9f516c306f02db026a40fedac9c88a4b0a9a2b
Bradley Gray <git@bradleygray.co.uk>
2026-02-26 00:31:00 +0000

    patched django version bug in surface mineral refresh migration

--- dj4xol/migrations/0057_regenerate_unowned_star_minerals.py
+++ dj4xol/migrations/0057_regenerate_unowned_star_minerals.py
@@ -9,29 +9,14 @@ from dj4xol import mineral_rules
 def regenerate_unowned_star_minerals(apps, schema_editor):
     Star = apps.get_model('dj4xol', 'Star')
 
-    unowned_stars = Star.objects.filter(player__isnull=True).only('id')
-    updates = []
-
-    for star in unowned_stars:
-        star.ironium_yield = mineral_rules.random_ironium_yield()
-        star.boranium_yield = mineral_rules.random_boranium_yield()
-        star.germanium_yield = mineral_rules.random_germanium_yield()
-        star.ironium_inventory = mineral_rules.random_surface_ironium_init()
-        star.boranium_inventory = mineral_rules.random_surface_boranium_init()
-        star.germanium_inventory = mineral_rules.random_surface_germanium_init()
-        updates.append(star)
-
-    if updates:
-        Star.objects.bulk_update(
-            updates,
-            [
-                'ironium_yield',
-                'boranium_yield',
-                'germanium_yield',
-                'ironium_inventory',
-                'boranium_inventory',
-                'germanium_inventory',
-            ],
+    for star in Star.objects.filter(player__isnull=True).only('id'):
+        Star.objects.filter(pk=star.pk).update(
+            ironium_yield=mineral_rules.random_ironium_yield(),
+            boranium_yield=mineral_rules.random_boranium_yield(),
+            germanium_yield=mineral_rules.random_germanium_yield(),
+            ironium_inventory=mineral_rules.random_surface_ironium_init(),
+            boranium_inventory=mineral_rules.random_surface_boranium_init(),
+            germanium_inventory=mineral_rules.random_surface_germanium_init(),
         )
 
 
